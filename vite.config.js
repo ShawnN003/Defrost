2406f5ebf42138920694f177af87b78bd481d4df
ShawnN003 <nguru.shawn@student.greenriver.edu>
2026-03-07 16:56:51 -0800

    Bypassing vm security issue

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: 'all',
+  },
   test: {
     environment: "jsdom",
     include: ["src/**/*.test.{js,jsx}"]
